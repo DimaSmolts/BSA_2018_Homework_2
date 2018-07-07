9f4508d33251e7cb796289ed3df2db54750fdf07
Dmytro <dima.smolts@gmail.com>
2018-07-07 16:15:54 +0300

    Controller and simple view for Post (fixes #5)

---
+++ Project2/Project2/Controllers/PostController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Project2.Controllers
+{
+    public class PostController : Controller
+    {
+        public IActionResult Index(int id)
+        {
+			ViewData["PostId"] = id;
+			return View();
+        }
+    }
+}
