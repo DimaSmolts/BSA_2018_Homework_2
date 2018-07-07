a961a3629b5e9f8bea836f5593f6e8f5f18bac24
Dmytro <dima.smolts@gmail.com>
2018-07-07 13:12:14 +0300

    Controller and simple view for User (fixes #4)

---
+++ Project2/Project2/Controllers/UserController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Project2.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index(int id)
+        {
+			ViewData["UserId"] = id;
+            return View();
+        }
+    }
+}
