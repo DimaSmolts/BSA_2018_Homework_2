837daf9830256c16ff965af95f76873fca5205ed
Dmytro <dima.smolts@gmail.com>
2018-07-07 18:02:09 +0300

    Controller and simple view for Post (fixes #6)

---
+++ Project2/Project2/Controllers/TodoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Project2.Controllers
+{
+    public class TodoController : Controller
+    {
+        public IActionResult Index(int id)
+        {
+			ViewData["TodoId"] = id;
+			return View();
+        }
+    }
+}
